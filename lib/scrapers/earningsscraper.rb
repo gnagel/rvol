cb78217ad675b8b6fad8ca01572de8b1c75a33d3
tonik <tonikarhu@gmail.com>
2010-12-22 20:59:49 +0200

    removed puts messages

--- lib/scrapers/earningsscraper.rb
+++ lib/scrapers/earningsscraper.rb
@@ -6,9 +6,12 @@ require "scrapers/optionschainsscraper"
 require "core/dateutil"
 require 'typhoeus'
 
+#
+# Load earnings for coming month
+#
 class EarningsScraper
   #
-  # Earnings scraper
+  # Load all earnings , store only the ones in the filter
   #
   def getEarningsMonth2(filter)
     hydra = Typhoeus::Hydra.new(:max_concurrency => 20)
@@ -16,21 +19,18 @@ class EarningsScraper
     t = DateTime.now
     t.step(t+30,step=1) { | n |
       date = n.strftime("%Y%m%d")
-
       url="http://biz.yahoo.com/research/earncal/"+date+".html"
-      puts url
-      request = Typhoeus::Request.new(url)
 
+      request = Typhoeus::Request.new(url)
       request.on_complete { | response |
         begin
           if(response.code==200)
-            puts 'success 200'
+            puts 'HTTP RESPONSE: 200 for Date: '+date
             doc = Hpricot(response.body)
             chains = doc.search("//a[@href]")
             for obj in chains
               if obj.to_html.include? "http://finance.yahoo.com/q?"
                 ticker = obj.inner_text
-                puts obj.inner_text
                 if filter.include?(ticker)
                   earning = Earning.new(date,ticker)
                   boolean = earning.save
@@ -38,21 +38,16 @@ class EarningsScraper
               end
             end
           else
-            puts 'failed'
-            puts response.code
+            puts 'HTTP RESPONSE: '+response.code.to_s
             #puts response.body
           end
         rescue => boom
           puts 'failed for date' +date
           puts boom
         end
-      }
+      } # end request
       hydra.queue(request)
     }
-    p 'starting hydra'
     hydra.run
-    p 'hydra done'
-
   end
-
 end
