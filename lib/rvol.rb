0541dc6a3def3a7e5e73e55b42480ecb4182d0b5
tonik <tonikarhu@gmail.com>
2011-03-30 16:22:27 +0300

    fixed database problem

--- lib/rvol.rb
+++ lib/rvol.rb
@@ -11,21 +11,27 @@ require 'yaml'
 #
 module Rvol
   # All applcation configurations are in this file
-    @@config = YAML.load_file File.join(File.dirname(__FILE__),'config.yml')
-  
-  # Version 
-    @@version = IO.readlines(File.join(File.dirname(__FILE__),'../VERSION'))
+  @@config = YAML.load_file File.join(File.dirname(__FILE__),'config.yml')
+
+  # Version
+  @@version = IO.readlines(File.join(File.dirname(__FILE__),'../VERSION'))
   # Returns config for use in all sub classes
   def Rvol.config
     @@config
   end
-  
+
   def version
-     @@version
+    @@version
   end
   # default adater for all db action stored in gem home directory
-  snapshot = 'sqlite://' + File.join(ENV['GEM_HOME'],'gems','rvol-'+@@version[0])+'/'+Rvol.config['snapshot']
+  #snapshot = 'sqlite://' + File.join(ENV['GEM_HOME'],'gems','rvol-'+@@version[0])+'/'+Rvol.config['snapshot']
+  if !File.exists?(ENV['HOME']+'/.rvol')
+    Dir.mkdir(ENV['HOME']+'/.rvol')
+  end
+  snapshot = 'sqlite://'+ENV['HOME']+'/.rvol/'+Rvol.config['snapshot']
   DataMapper.setup(:default,snapshot)
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
 
   def earningsReport
     EarningsReport.new.generateReport
@@ -66,7 +72,7 @@ module Rvol
   def chainReportTop10VolPuts
     ChainsReport.new.generateReportTop10Puts
   end
-  
+
   def reportsdev20
     Sdreport.new.generateReportTop50StandardDeviation
   end
@@ -85,5 +91,5 @@ module Rvol
   def runCron
     Cron.new.run
   end
-  
+
 end
